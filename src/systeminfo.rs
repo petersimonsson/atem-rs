26765894b2f0adadc4a1330caac407705b400ded
Peter Simonsson <peter.simonsson@gmail.com>
2023-10-24 20:51:28 +0200

    Use bitflags for bitmasks

--- src/systeminfo.rs
+++ src/systeminfo.rs
@@ -1,5 +1,6 @@
 use core::fmt;
 
+use bitflags::bitflags;
 use bytes::{Buf, Bytes};
 
 use crate::parser::{self, parse_str};
@@ -112,16 +113,200 @@ impl fmt::Display for SourceType {
     }
 }
 
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct InputFlags: u16 {
+        const SDI = 0x0001;
+        const HDMI = 0x0002;
+        const COMPOSITE = 0x0004;
+        const COMPONENT = 0x0008;
+        const SVIDEO = 0x0010;
+        const INTERNAL = 0x0100;
+    }
+}
+
+impl fmt::Display for InputFlags {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut output = String::new();
+
+        if self.contains(InputFlags::SDI) {
+            output += "SDI";
+        }
+        if self.contains(InputFlags::HDMI) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "HDMI";
+        }
+        if self.contains(InputFlags::COMPOSITE) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "Composite";
+        }
+        if self.contains(InputFlags::COMPONENT) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "Component";
+        }
+        if self.contains(InputFlags::SVIDEO) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "S-Video";
+        }
+        if self.contains(InputFlags::INTERNAL) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "Internal";
+        }
+
+        write!(f, "{}", output)
+    }
+}
+
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct FunctionFlags: u8 {
+        const AUXILIARY = 0x01;
+        const MULTIVIEWER = 0x02;
+        const SUPERSOURCE_ART = 0x04;
+        const SUPERSOURCE_BOX = 0x08;
+        const KEY_SOURCES = 0x10;
+    }
+}
+
+impl fmt::Display for FunctionFlags {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut output = String::new();
+
+        if self.contains(FunctionFlags::AUXILIARY) {
+            output += "Auxiliary";
+        }
+        if self.contains(FunctionFlags::MULTIVIEWER) {
+            if !output.is_empty() {
+                output += ", "
+            }
+
+            output += "Multiviewer";
+        }
+        if self.contains(FunctionFlags::SUPERSOURCE_ART) {
+            if !output.is_empty() {
+                output += ", "
+            }
+
+            output += "SuperSource Art";
+        }
+        if self.contains(FunctionFlags::SUPERSOURCE_BOX) {
+            if !output.is_empty() {
+                output += ", "
+            }
+
+            output += "SuperSource Box";
+        }
+        if self.contains(FunctionFlags::KEY_SOURCES) {
+            if !output.is_empty() {
+                output += ", "
+            }
+
+            output += "Key Sources";
+        }
+
+        write!(f, "{}", output)
+    }
+}
+
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct MixEffectFlags: u8 {
+        const ME1 = 0x01;
+        const ME2 = 0x02;
+        const ME3 = 0x04;
+        const ME4 = 0x08;
+        const ME5 = 0x10;
+        const ME6 = 0x20;
+        const ME7 = 0x40;
+        const ME8 = 0x80;
+    }
+}
+
+impl fmt::Display for MixEffectFlags {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut output = String::new();
+        if self.contains(MixEffectFlags::ME1) {
+            output += "ME1";
+        }
+        if self.contains(MixEffectFlags::ME2) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "ME2";
+        }
+        if self.contains(MixEffectFlags::ME3) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "ME3";
+        }
+        if self.contains(MixEffectFlags::ME4) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "ME4";
+        }
+        if self.contains(MixEffectFlags::ME5) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "ME5";
+        }
+        if self.contains(MixEffectFlags::ME6) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "ME6";
+        }
+        if self.contains(MixEffectFlags::ME7) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "ME7";
+        }
+        if self.contains(MixEffectFlags::ME8) {
+            if !output.is_empty() {
+                output += ", ";
+            }
+
+            output += "ME8";
+        }
+
+        write!(f, "{}", output)
+    }
+}
+
 #[derive(Debug)]
 pub struct Source {
     id: u16,
     name: Option<String>,
     short_name: Option<String>,
-    available_inputs: u16,
+    available_inputs: InputFlags,
     active_input: Option<Input>,
     source_type: SourceType,
-    available_functions: u8,
-    available_on_me: u8,
+    available_functions: FunctionFlags,
+    available_on_me: MixEffectFlags,
 }
 
 impl Source {
@@ -130,13 +315,12 @@ impl Source {
         let name = parse_str(&mut data.split_to(20))?;
         let short_name = parse_str(&mut data.split_to(4))?;
         data.get_u16(); // Skip 2 bytes
-        let available_inputs = data.get_u16();
+        let available_inputs = InputFlags::from_bits(data.get_u16()).unwrap();
         let active_input = Input::from_u16(data.get_u16());
         let source_type = SourceType::from_u8(data.get_u8()).unwrap();
         data.get_u8(); // Skip byte
-        let available_functions = data.get_u8(); // Bit 0: Auxiliary, 1: Multiviewer, 2: SuperSource Art,
-                                                 // 3: SuperSource Box, 4: Key Sources
-        let available_on_me = data.get_u8(); // Bit 0: ME1 + Fill Sources, 1: ME2 + Fill Sources
+        let available_functions = FunctionFlags::from_bits(data.get_u8()).unwrap();
+        let available_on_me = MixEffectFlags::from_bits(data.get_u8()).unwrap();
 
         Ok(Source {
             id,
@@ -149,61 +333,19 @@ impl Source {
             available_on_me,
         })
     }
-
-    fn available_inputs_str(&self) -> String {
-        let mut res = String::new();
-
-        if self.available_inputs & 0x0001 > 0 {
-            res += "SDI";
-        }
-
-        if self.available_inputs & 0x0002 > 0 {
-            if !res.is_empty() {
-                res += ", ";
-            }
-
-            res += "HDMI";
-        }
-
-        if self.available_inputs & 0x0004 > 0 {
-            if !res.is_empty() {
-                res += ", ";
-            }
-
-            res += "Composite";
-        }
-
-        if self.available_inputs & 0x0008 > 0 {
-            if !res.is_empty() {
-                res += ", ";
-            }
-
-            res += "Component";
-        }
-
-        if self.available_inputs & 0x0010 > 0 {
-            if !res.is_empty() {
-                res += ", ";
-            }
-
-            res += "S-Video";
-        }
-
-        res
-    }
 }
 
 impl fmt::Display for Source {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let input_str = if let Some(input) = &self.active_input {
-            format!("[{}] -> {}", self.available_inputs_str(), input)
+            format!("[{}] -> {}, ", self.available_inputs, input)
         } else {
             "".to_string()
         };
 
         write!(
             f,
-            "Source {}: {} ({}), {}, {}, [{:08b}], [{:08b}]",
+            "Source {}: {} ({}), {}{}, [{}], [{}]",
             self.id,
             self.name.as_deref().unwrap_or(""),
             self.short_name.as_deref().unwrap_or(""),
