0eaa33393226a4f5550a7580787573eefbd759a7
Peter Simonsson <peter.simonsson@gmail.com>
2025-09-28 06:36:28 +0200

    Add support for keyer base properties

--- src/command.rs
+++ src/command.rs
@@ -2,7 +2,7 @@ use std::fmt::Display;
 
 use bytes::{Buf, Bytes};
 use thiserror::Error;
-use tracing::debug;
+use tracing::{debug, info};
 
 use crate::{
     multiview::{MultiViewInput, MultiViewLayout, MultiViewSafeArea, MultiViewVU},
@@ -61,6 +61,7 @@ pub enum Command {
     TransitionDVE(TransitionDVE),
     TransitionStinger(TransitionStinger),
     KeyerOnAir(KeyerOnAir),
+    KeyerBaseProperties(KeyerBaseProperties),
 }
 
 impl Command {
@@ -191,6 +192,11 @@ impl Command {
                 let keyer_on_air = KeyerOnAir::parse(&mut data);
                 Ok(Command::KeyerOnAir(keyer_on_air))
             }
+            b"KeBP" => {
+                info!("KeBP Data: {:02X?} [{}]", &data[..], data_size);
+                let keyer_bp = KeyerBaseProperties::parse(&mut data);
+                Ok(Command::KeyerBaseProperties(keyer_bp))
+            }
             _ => {
                 let cmd_str = String::from_utf8(cmd.to_vec())?;
                 debug!(
@@ -239,6 +245,7 @@ impl Display for Command {
             Command::TransitionDVE(dve) => write!(f, "Transition DVE: {dve}"),
             Command::TransitionStinger(stinger) => write!(f, "Transition stinger: {stinger}"),
             Command::KeyerOnAir(on_air) => write!(f, "Keyer on air: {on_air}"),
+            Command::KeyerBaseProperties(bp) => write!(f, "Keyer BP: {bp}"),
         }
     }
 }
@@ -317,6 +324,62 @@ impl Display for KeyerOnAir {
             f,
             "ME: {} Keyer: {} On air: {}",
             self.me, self.keyer, self.on_air
+        )
+    }
+}
+
+pub struct KeyerBaseProperties {
+    me: u8,
+    keyer: u8,
+    keyer_type: u8,
+    flying: bool,
+    fill: u16,
+    key: u16,
+    mask: bool,
+    mask_top: f32,
+    mask_bottom: f32,
+    mask_left: f32,
+    mask_right: f32,
+}
+
+impl KeyerBaseProperties {
+    pub fn parse(data: &mut Bytes) -> Self {
+        let me = data.get_u8();
+        let keyer = data.get_u8();
+        let keyer_type = data.get_u8();
+        data.get_u16(); // skip 2 bytes, unknown
+        let flying = data.get_u8() > 0;
+        let fill = data.get_u16();
+        let key = data.get_u16();
+        let mask = data.get_u8() > 0;
+        data.get_u8(); // skip, unknown
+        let mask_top = data.get_i16() as f32 / 1000.0;
+        let mask_bottom = data.get_i16() as f32 / 1000.0;
+        let mask_left = data.get_i16() as f32 / 1000.0;
+        let mask_right = data.get_i16() as f32 / 1000.0;
+
+        Self {
+            me,
+            keyer,
+            keyer_type,
+            flying,
+            fill,
+            key,
+            mask,
+            mask_top,
+            mask_bottom,
+            mask_left,
+            mask_right,
+        }
+    }
+}
+
+impl Display for KeyerBaseProperties {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "ME: {} Keyer: {} Type: {} Flying: {} Fill: {} Key: {} Mask: {} Mask top: {:.2} Mask bottom: {:.2} Mask left: {:.2} Mask right: {:.2}",
+            self.me, self.keyer, self.keyer_type, self.flying, self.fill, self.key, self.mask, self.mask_top, self.mask_bottom, self.mask_left, self.mask_right
         )
     }
 }
